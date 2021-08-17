bf1df0a9163a875d0e60692ee4cb86793619aeb6
Julie Winchester <julie.winchester@duke.edu>
2021-08-17 17:06:03 -0500

    Build initial version of site

---
+++ build/precache-manifest.1f2805e206746632c805ef6001d2fa6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3117b8ca6205c98afc1dc460278f840b",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "f27b47470191d75b70c5",
+    "url": "/dnd/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "41d6b8106bb7823b9d9d",
+    "url": "/dnd/static/css/main.df5abb71.chunk.css"
+  },
+  {
+    "revision": "f27b47470191d75b70c5",
+    "url": "/dnd/static/js/2.83e7a7bb.chunk.js"
+  },
+  {
+    "revision": "41d6b8106bb7823b9d9d",
+    "url": "/dnd/static/js/main.e5f89628.chunk.js"
+  },
+  {
+    "revision": "f459ec4b6944d25afa2f",
+    "url": "/dnd/static/js/runtime~main.c70589ab.js"
+  },
+  {
+    "revision": "398e9770dfb4eb1ddccd3b73f8844a7a",
+    "url": "/dnd/static/media/nodesto-caps-condensed.398e9770.otf"
+  }
+]);
